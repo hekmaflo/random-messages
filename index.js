4473290b116f4c42d33e2d7cda6ae3e7ae219c9f
Hector <hmflores88@gmail.com>
2024-01-30 22:18:04 -0600

    added a random number generator

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
 console.log("hello world")
 
+const intro ={
+    partOne: ["Hello there, ", "Goodbye,", "How do you feel about"]
+};
+
 const messages ={
     partOne: ["hello", "goodbye", "hasta luego"],
     partTwo: ["boo", "baa", "haa"],
     partThree: ["one", "two", "three"]
 };
 
-function getRandom() {
+function getRandom(array) {
+    length = array.length
 
-    return Math.round(Math.random() * 10);
+    return Math.floor(Math.random() * length);
   }
 
-  console.log(getRandom());
+const numOfKeys = Object.keys(messages);
 
-  console.log(messages.partOne.length)
+const count = numOfKeys.length;
+
+const finalOne = intro.partOne[getRandom(intro.partOne)];
+
+const finalTwo = messages.partOne[getRandom(messages.partOne)];
+
+
+console.log(finalOne + finalTwo);
+console.log(getRandom(intro.partOne));
+console.log(getRandom(messages.partOne));
+console.log(numOfKeys);
+console.log(count);
+
+
+
+
+
+
+
+
+
+//   console.log(getRandom(messages.partOne.length));
+
+//   console.log(messages.partOne.length)
