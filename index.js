962092904a3c81c8d3dd2f109f65d285fb8d0f6c
Hector <hmflores88@gmail.com>
2024-01-30 18:22:26 -0600

    small function test

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 console.log("hello world")
+
+const messages ={
+    partOne: ["hello", "goodbye", "hasta luego"],
+    partTwo: ["boo", "baa", "haa"],
+    partThree: ["one", "two", "three"]
+};
+
+function getRandom() {
+
+    return Math.round(Math.random() * 10);
+  }
+
+  console.log(getRandom());
+
+  console.log(messages.partOne.length)
